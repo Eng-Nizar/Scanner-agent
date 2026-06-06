103941366d0b69435325d3bf68ea60c25dec0cd1
Eng-Nizar <46514262+Eng-Nizar@users.noreply.github.com>
2026-06-06 03:39:24 +0300

    Add files via upload

---
+++ app_INIT.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+    
+    # Import routes
+    from app_routes import main_bp, api_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp)
+    
+    return app
